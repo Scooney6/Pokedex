2079c3018f5606f1c4fe06d19d193c0c3b6462f2
Scooney6 <cooney.samuel@gmail.com>
2021-07-23 04:31:53 -0400

    start pokepy implementation. create pokedex page

--- Scooney6/app.py
+++ Scooney6/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+import pokepy
+from flask import Flask, render_template, url_for, request
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # TODO: manually request all pokedexes and display them
+    # TODO: clicking on a pokedex pulls up the pokemon in that pokedex
+    # TODO: add a search bar to search pokemon or pokedexes
+    pk = pokepy.V2Client().get_pokedex(1)
+    print(pk[0].pokemon_entries[0].pokemon_species.name)
+    return redirect(url_for("pokedex", name="national"))
+
+
+@app.route("/pokedex", methods=["POST", "GET"])
+def pokedex():
+    pokedex = pokepy.V2Client().get_pokedex(request.args.get('name'))
+    pokeobjs = pokedex[0].pokemon_entries
+    pokemon = []
+    i = 0
+    for pokemons in pokeobjs:
+        pokemon.append(pokeobjs[i].pokemon_species.name)
+        i = i + 1
+    return render_template("pokedex.html", pokemon=pokemon, pokedex=pokedex[0].name)
 
 
 if __name__ == '__main__':
