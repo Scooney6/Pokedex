cac01e57acd9fa03beedb5c6b0e8d29c2ae4b09d
Scooney6 <cooney.samuel@gmail.com>
2021-08-12 03:53:56 -0400

    implement caching and get version groups

--- Scooney6/app.py
+++ Scooney6/app.py
@@ -1,18 +1,28 @@
 import pokepy
+import requests
 from flask import Flask, render_template, url_for, request
 from werkzeug.utils import redirect
+from functools import lru_cache
 
 app = Flask(__name__)
 
 
+@lru_cache
+def api_call(url):
+    print("fetching")
+    data = requests.get(url).json()
+    return data
+
+
 @app.route("/")
 def index():
-    # TODO: manually request all pokedexes and display them
+    # TODO: manually request all versions and display them
+    return render_template("index.html", versions=[versiongroup["name"] for versiongroup in api_call("https://pokeapi.co/api/v2/version-group/")["results"]])
     # TODO: clicking on a pokedex pulls up the pokemon in that pokedex
     # TODO: add a search bar to search pokemon or pokedexes
-    pk = pokepy.V2Client().get_pokedex(1)
-    print(pk[0].pokemon_entries[0].pokemon_species.name)
-    return redirect(url_for("pokedex", name="national"))
+    #pk = pokepy.V2Client().get_pokedex(1)
+    #print(pk[0].pokemon_entries[0].pokemon_species.name)
+    #return redirect(url_for("pokedex", name="national"))
 
 
 @app.route("/pokedex", methods=["POST", "GET"])
